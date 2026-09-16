fa2906d0ce83963836095f45ff32eb34bc2c8725
bananaman4262arada <arada.hd@obmontessori.edu.ph>
2026-09-17 06:41:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 from pyscript import document, display
         
 def create_order(e):
-       document.getElementById("output2").innerHTML ="" #clears previous result
-
-       prod1=document.getElementById("item1") #get item 1 id
-       #Calculate
-       subtotal=float(prod1.value) * prod1.checked 
-       size = document.querySelector("input[name='size']:checked")
-       price = float(size.value) 
-       grandtotal = subtotal + price
-       display(grandtotal, target="output2")
-
-def place_order(e):
-       document.getElementById("output3").innerHTML ="" #clears previous result
-       coffee = document.getElementById("coffee")
-       coffee_price = float(coffee.value)
-       display(coffee_price, target="output3") #display coffee price
-
-def show_order(e):
-       document.getElementById("output4").innerHTML = "" # clears previous result
-       prod1=document.getElementById("item1") #get item 1 id
-       subtotal= float(prod1.value) * prod1.checked
-       size = document.querySelector("input[name='size']:checked")
-       price = float(size.value)
-       grandtotal = subtotal + price
-       coffee = document.getElementById("coffee")
-       coffee_price = float(coffee.value)
-       final_order = grandtotal + coffee_price
-       display(f'You have to pay a total of {final_order}', target="output4")
+       document.getElementById("output1").innerHTML = "" # clears previous result
+       item1 = document.getElementById("item1")
+       item2 = document.getElementById("item2")
+       item3 = document.getElementById("item3")
+       item4 = document.getElementById("item4")
+       item5 = document.getElementById("item5")
+       subtotal = (float(item1.value) * item1.checked) + (float(item2.value) * item2.checked) + (float(item3.value) * item3.checked) + (float(item4.value) * item4.checked) + (float(item5.value) * item5.checked)
+       tax = subtotal * 0.12
+       grandtotal = subtotal + tax
+       display(f'SUBTOTAL               {subtotal:}', target="output1", append=True)
+       display(f'SALES TAX ON 7.34      {tax:}', target="output1", append=True)
+       display(f'TOTAL DUE              {grandtotal:}', target="output1", append=True)order}', target="output4")
